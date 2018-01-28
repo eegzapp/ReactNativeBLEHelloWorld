92d205247cb6016f984aca69c801c19469c3095a
mike <mike.egan@gmail.com>
2018-01-27 16:50:22 -0800

    bluetooth libraries added

--- App.js
+++ App.js
@@ -9,8 +9,10 @@ import {
   Platform,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -18,6 +20,40 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+
+const btInit = () => {
+  BleManager.start({showAlert: true})
+  .then(()=>{
+    console.log('BLE Module Initialized')
+  })
+}
+
+const btScan = () => {
+  BleManager.scan([], 15, true)
+  .then(()=>{
+    console.log('scan started')
+  })
+}
+
+const btReadMac = () => {
+  BleManager.readRSSI('A441DCCE-5C0E-4E42-A1D9-3A7421223FFC')
+    .then((rssi) => {
+      console.log(`Current MAC: ${rssi}`);
+    })
+}
+
+const btConnect = () => {
+  BleManager.connect('A441DCCE-5C0E-4E42-A1D9-3A7421223FFC')
+  //BleManager.connect('37D6F7BD-C2FC-49AE-85A6-BED24D8A2CED')
+    .then(() => {
+      console.log('connected');
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
 
 export default class App extends Component<{}> {
   render() {
@@ -32,6 +68,10 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Button title="init" onPress={btInit}></Button>
+        <Button title="scan" onPress={btScan}></Button>
+        <Button title="connect" onPress={btConnect}></Button>
+        <Button title="read MAC" onPress={btReadMac}></Button>
       </View>
     );
   }
